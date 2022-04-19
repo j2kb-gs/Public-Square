943db9dc33b21ca906c9e4011aa4acd5e6c62696
j2kb-gs <90418846+j2kb-gs@users.noreply.github.com>
2022-04-19 16:42:20 -0400

    layout tried changes

---
+++ NYC_Public_Wifi/NYC_Public_Wifi/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for NYC_Public_Wifi project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NYC_Public_Wifi.settings')
+
+application = get_asgi_application()
