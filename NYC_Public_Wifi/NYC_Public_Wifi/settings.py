236874310fab9214a310e4eeade443ae3edcf040
ej0306 <54604143+ej0306@users.noreply.github.com>
2023-05-22 16:26:54 -0400

    Update settings.py

--- NYC_Public_Wifi/NYC_Public_Wifi/settings.py
+++ NYC_Public_Wifi/NYC_Public_Wifi/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-^8=_moasq6cu4=5n0(g$o)er8ui()vhj%udkw)@y2(-xkhi2!a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
